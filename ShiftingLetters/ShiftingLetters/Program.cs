1e914889193db4d4e92394265b5963f79cee25f0
Rehan <rehanboyaju523@gmail.com>
2025-07-03 09:57:18 +0545

    Brute force

---
+++ ShiftingLetters/ShiftingLetters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace ShiftingLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ShiftingLetters("abc",new int[] {3,5,9}));
+        }
+
+
+        static string ShiftingLetters(string s, int[] shifts)
+        {
+            int n = shifts.Length;
+            int[] sum = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    sum[j] = (sum[j] + shifts[i]) % 26;
+                }
+
+            }
+            StringBuilder sb = new StringBuilder(s);
+            for (int i = 0; i < s.Length; i++)
+            {
+                sb[i] = (char)((((sb[i] - 'a') + sum[i]) % 26) + 'a');
+            }
+
+            return sb.ToString();
+
+
+
+        }
+    }
+}
