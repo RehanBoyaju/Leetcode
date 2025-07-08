5c7885ce475b59a09e8a53c0506642d52005a631
Rehan <rehanboyaju523@gmail.com>
2025-07-08 13:46:53 +0545

    DP with binary search soln

--- Maximum Number of Events That Can Be Attended II/Maximum Number of Events That Can Be Attended II/Program.cs
+++ Maximum Number of Events That Can Be Attended II/Maximum Number of Events That Can Be Attended II/Program.cs
@@ -7,25 +7,50 @@ public class Solution
         Array.Sort(events, (a, b) => a[0] - b[0]);
 
         int n = events.Length;
+        int[][] dp = new int[n + 1][];
+        for (int i = 0; i < n; i++)
+        {
+            dp[i] = new int[k + 1];
+            Array.Fill(dp[i], -1);
+        }
 
-        int Solve(int i, int count, int endDay)
+        int Solve(int i, int count)
         {
             if (i == n || count == k)
             {
                 return 0;
             }
-
-            int take = int.MinValue;
-            if (events[i][0] > endDay)
+            if (dp[i][count] != -1)
             {
-                take = events[i][2] + Solve(i + 1, count + 1, events[i ][1]);
+                return dp[i][count];
             }
 
-            int skip = Solve(i + 1, count, endDay);
+            int next = BinarySearch(i + 1, events[i][1]);
 
-            return Math.Max(take, skip);
+            int take = events[i][2] + Solve(next, count + 1);
+            int skip = Solve(i + 1, count);
+            int res = Math.Max(take, skip);
+
+            dp[i][count] = res;
+            return res;
         }
-
-        return Solve(0, 0, 0);
+        int BinarySearch(int left, int prevEnd)
+        {
+            int right = n;
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                if (events[mid][0] > prevEnd)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+        return Solve(0, 0);
     }
 }
--- ShiftingLetters/ShiftingLetters/Program.cs
+++ ShiftingLetters/ShiftingLetters/Program.cs
@@ -13,19 +13,17 @@ namespace ShiftingLetters
         static string ShiftingLetters(string s, int[] shifts)
         {
             int n = shifts.Length;
-            int[] sum = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j <= i; j++)
-                {
-                    sum[j] = (sum[j] + shifts[i]) % 26;
-                }
+            int[] suffixSum = new int[n];
 
+            suffixSum[n - 1] = shifts[n - 1];
+            for (int i = n - 2; i >= 0; i--)
+            {
+                suffixSum[i] = (shifts[i]+ suffixSum[i + 1]) % 26;
             }
             StringBuilder sb = new StringBuilder(s);
-            for (int i = 0; i < s.Length; i++)
+            for (int i = 0; i < n; i++)
             {
-                sb[i] = (char)((((sb[i] - 'a') + sum[i]) % 26) + 'a');
+                sb[i] = (char)('a' + (sb[i]-'a'+suffixSum[i]) %26);
             }
 
             return sb.ToString();
