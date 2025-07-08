401186e50af04e302eea17458564842939e7c136
Rehan <rehanboyaju523@gmail.com>
2025-07-08 13:48:43 +0545

    Fix yesterday commit

---
+++ FindingPairsWithSum/FindingPairsWithSum/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace FindingPairsWithSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindSumPairs s = new FindSumPairs(new int[] { 1, 1, 2, 2, 2, 3 } , new int[] { 1, 4, 5, 2, 5, 4 });
+            Console.WriteLine(s.Count(7));
+            s.Add(3,2);
+            Console.WriteLine(s.Count(8));
+            Console.WriteLine(s.Count(4));
+
+
+
+        }
+        public class FindSumPairs
+        {
+            private List<int> nums1;
+            private List<int> nums2;
+            Dictionary<int, int> dict;
+            public FindSumPairs(int[] nums1, int[] nums2)
+            {
+
+                dict = new();
+                this.nums1 = nums1.ToList();
+                this.nums2 = nums2.ToList();
+
+                foreach (int num in nums2)
+                {
+                    if (dict.ContainsKey(num))
+                    {
+                        dict[num]++;
+                    }
+                    else
+                    {
+                        dict[num] = 1;
+                    }
+                }
+
+            }
+
+            public void Add(int index, int val)
+            {
+
+                dict[nums2[index]]--;
+
+                nums2[index] += val;
+
+                dict[nums2[index]]++;
+
+            }
+
+            public int Count(int tot)
+            {
+                int count = 0;
+                foreach (int num in nums1)
+                {
+                    int key = tot - num;
+                    if (dict.ContainsKey(key))
+                    {
+                        count += dict[key];
+                    }
+                }
+                return count;
+            }
+        }
+        // 1 - 1
+        // 4 - 3
+        // 5 - 2
+        // 2 - 0
+
+        // 1,4,5,_4,5,4
+
+
+        //  nums1 = [1,1,2,2,2,3] => List
+        //  nums2 = [1,4,5,2,5,4] => Dictionary
+
+
+
+
+
+
+    }
+}
---
+++ MaxEventsToAttend/MaxEventsToAttend/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+Solution s = new Solution();
+
+
+Console.WriteLine(s.MaxEvents([[1, 2], [2, 2], [3, 3], [3, 4], [3, 4]]));
+public class Solution
+{
+    public int MaxEvents(int[][] events)
+    {
+        int n = events.Length;
+        int maxDay = 0;
+
+        foreach(var e in events)
+        {
+            maxDay = Math.Max(maxDay, e[1]);
+        }
+
+        Array.Sort(events, (a, b) => a[0].CompareTo(b[0]));
+
+        PriorityQueue<int, int> pq = new();
+        int count = 0;
+        for(int i = 1, j = 0; i <= maxDay; i++)
+        {
+            //Add the events that are currently ongoing
+            while(j<n && events[j][0] <= i)
+            {
+                pq.Enqueue(events[j][1], events[j][1]);
+                j++;
+            }
+
+            //Remove the events that are already pass their dates
+
+            while(pq.Count > 0 && pq.Peek() < i) 
+            {
+                
+                  pq.Dequeue();
+                
+            }
+            if(pq.Count > 0)
+            {
+                pq.Dequeue();
+                count++;
+            }
+        }
+
+        return count;
+        
+
+
+    }
+}
+//[[1,2],[1,2],[3,3],[1,5],[1,5]]
+
+
+//   1,2   1,2  
+//    1       2       3       4       5
